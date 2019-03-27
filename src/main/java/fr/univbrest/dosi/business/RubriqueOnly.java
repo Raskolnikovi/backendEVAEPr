1064324b3711544950b65d8340d579ddd92ed7b3
Yassine BHS <bhsyassine@gmail.com>
2019-03-27 18:27:19 +0100

    repo modif3

--- src/main/java/fr/univbrest/dosi/business/RubriqueOnly.java
+++ src/main/java/fr/univbrest/dosi/business/RubriqueOnly.java
@@ -3,6 +3,7 @@ package fr.univbrest.dosi.business;
 import fr.univbrest.dosi.bean.Rubrique;
 
 public interface RubriqueOnly {
+	int getIdRubriqueEvaluation();
 	Rubrique getRubrique();
 	String getDesignation();
 	byte getOrdre();
